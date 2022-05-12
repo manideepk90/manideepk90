5ac7f012340650a68bba4a947068f8659b482dea
KUNTIMADDI MANIDEEP <52228935+manideepk90@users.noreply.github.com>
2022-05-13 00:47:22 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const cookieParser = require("cookie-parser")
-require("./Database/db")
+require("./database/db")
 const path = require('path')
 const testRouter = require("./routers/testRouter")
 
